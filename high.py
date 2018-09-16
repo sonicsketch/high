ce72ffebd045a5ec928393cbe90d1d44206cd24c
sonicsketch <sonicsketch@gmail.com>
2018-09-16 15:36:05 +0900

    small update

--- high.py
+++ high.py
@@ -3,13 +3,13 @@ import os
 import time
 import highclass
 
-os.system('mode con:cols=162 lines=53')
+os.system('mode con:cols=163 lines=53')
 os.system('cls')
 
 
 f = open('total_map_path.txt', 'r')
-
 totalmap = f.read().split('\n')
+f.close()
 
 screencon = highclass.ScreenCon(totalmap)
 screencon.printscreen()
@@ -18,6 +18,8 @@ screencon.printscreen()
 #time.sleep(1)
 #screencon.printscreen()
 
-screencon.move(75, 40, 120, 40, 'ABCD')
+#screencon.move(75, 40, 120, 40, 'ABCD')
 
-f.close()
+s = input()
+
+print(s)
