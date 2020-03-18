0413ff489e278f054d0ac9acb608f8b6d173ec0d
sonicsketch <sonicsketch@gmail.com>
2020-03-18 22:57:48 +0900

    -

--- high.py
+++ high.py
@@ -7,33 +7,9 @@ import pyautogui as m
 os.system('mode con:cols=163 lines=53')
 os.system('cls')
 
-'''
-f = open('total_map_path.txt', 'r')
-totalmap = f.read().split('\n')
-f.close()
-
-screencon = highclass.ScreenCon(totalmap)
-screencon.printscreen()
-
-s = input()
-
-print(s)
-
-
-WIDTH = 162
-HEIGHT = 51
-
-tmap = [['o']*WIDTH for _ in range(HEIGHT)]
-
-for y in range(0, HEIGHT, 1):
-	for x in range(0, WIDTH, 1):
-		tmap[y][x] = totalmap[y][x:x+1]
-'''
-
 screencon = highclass.ScreenCon()
 screencon.loadmap('total_map_path.txt')
 screencon.printmap()
-
 
 #screencon.move(90, 40, 120, 40, '■')
 
@@ -44,8 +20,5 @@ try:
 except:
 	print('test')
 
-
-
 s = input()
-
 print(s)
